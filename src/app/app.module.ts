f6081c2bd7d777088f208973af93236fc40c8190
EdwinM96 <edwin.morales9b@gmail.com>
2019-04-15 15:14:15 -0600

    Added circle row component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { MainIndexComponent } from './components/main-index/main-index.component';
 import { CardRowComponent } from './components/card-row/card-row.component';
+import { CircleRowComponent } from './components/circle-row/circle-row.component';
+import { UpBtnComponent } from './components/up-btn/up-btn.component';
+import { MiddleSectionComponent } from './components/middle-section/middle-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarouselComponent,
     MainIndexComponent,
-    CardRowComponent
+    CardRowComponent,
+    CircleRowComponent,
+    UpBtnComponent,
+    MiddleSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/up-btn/up-btn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-up-btn',
+  templateUrl: './up-btn.component.html',
+  styleUrls: ['./up-btn.component.css']
+})
+export class UpBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
